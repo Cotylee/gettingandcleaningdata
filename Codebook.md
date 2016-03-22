f3f19691771d4a7640cf59353e7bd98fe68e47e4
cotylee <manegonel@gmail.com>
2016-03-22 09:43:32 -0400

    intial

---
+++ Codebook.md
@@ -0,0 +1,105 @@
+title: "codebook"
+author: "Coty Webb"
+date: "March 22, 2016"
+output: html_document
+
+The subjects are identified by number. Subject is used as an ID variable as is Activity
+
+
+ID Variables
+
+ "Subject"
+ "Activity"
+
+Measurment Variables
+
+ "tBodyAcc Mean  X"
+ "tBodyAcc Mean  Y"
+ "tBodyAcc Mean  Z"
+ "tBodyAcc StanDev  X"
+ "tBodyAcc StanDev  Y"
+ "tBodyAcc StanDev  Z"
+ "tGravityAcc Mean  X"
+ "tGravityAcc Mean  Y"
+ "tGravityAcc Mean  Z"
+ "tGravityAcc StanDev  X"
+ "tGravityAcc StanDev  Y"
+ "tGravityAcc StanDev  Z"
+ "tBodyAccJerk Mean  X"
+ "tBodyAccJerk Mean  Y"
+ "tBodyAccJerk Mean  Z"
+ "tBodyAccJerk StanDev  X"
+ "tBodyAccJerk StanDev  Y"
+ "tBodyAccJerk StanDev  Z"
+ "tBodyGyro Mean  X"
+ "tBodyGyro Mean  Y"
+ "tBodyGyro Mean  Z"
+ "tBodyGyro StanDev  X"
+ "tBodyGyro StanDev  Y"
+ "tBodyGyro StanDev  Z"
+ "tBodyGyroJerk Mean  X"
+ "tBodyGyroJerk Mean  Y"
+ "tBodyGyroJerk Mean  Z"
+ "tBodyGyroJerk StanDev  X"
+ "tBodyGyroJerk StanDev  Y"
+ "tBodyGyroJerk StanDev  Z"
+ "tBodyAccMag Mean "
+ "tBodyAccMag StanDev "
+ "tGravityAccMag Mean "
+ "tGravityAccMag StanDev "
+ "tBodyAccJerkMag Mean "
+ "tBodyAccJerkMag StanDev "
+ "tBodyGyroMag Mean "
+ "tBodyGyroMag StanDev "
+ "tBodyGyroJerkMag Mean "
+ "tBodyGyroJerkMag StanDev "
+ "fBodyAcc Mean  X"
+ "fBodyAcc Mean  Y"
+ "fBodyAcc Mean  Z"
+ "fBodyAcc StanDev  X"
+ "fBodyAcc StanDev  Y"
+ "fBodyAcc StanDev  Z"
+ "fBodyAcc Mean Freq X"
+ "fBodyAcc Mean Freq Y"
+ "fBodyAcc Mean Freq Z"
+ "fBodyAccJerk Mean  X"
+ "fBodyAccJerk Mean  Y"
+ "fBodyAccJerk Mean  Z"
+ "fBodyAccJerk StanDev  X"
+ "fBodyAccJerk StanDev  Y"
+ "fBodyAccJerk StanDev  Z"
+ "fBodyAccJerk Mean Freq X"
+ "fBodyAccJerk Mean Freq Y"
+ "fBodyAccJerk Mean Freq Z"
+ "fBodyGyro Mean  X"
+ "fBodyGyro Mean  Y"
+ "fBodyGyro Mean  Z"
+ "fBodyGyro StanDev  X"
+ "fBodyGyro StanDev  Y"
+ "fBodyGyro StanDev  Z"
+ "fBodyGyro Mean Freq X"
+ "fBodyGyro Mean Freq Y"
+ "fBodyGyro Mean Freq Z"
+ "fBodyAccMag Mean "
+ "fBodyAccMag StanDev "
+ "fBodyAccMag Mean Freq"
+ "fBodyBodyAccJerkMag Mean "
+ "fBodyBodyAccJerkMag StanDev "
+ "fBodyBodyAccJerkMag Mean Freq"
+ "fBodyBodyGyroMag Mean "
+ "fBodyBodyGyroMag StanDev "
+ "fBodyBodyGyroMag Mean Freq"
+ "fBodyBodyGyroJerkMag Mean "
+ "fBodyBodyGyroJerkMag StanDev "
+ "fBodyBodyGyroJerkMag Mean Freq"
+
+The Activity variable describes the activity being measured as proformed by each subject
+and takes values of:
+
+ LAYING
+ SITTING
+ STANDING
+ WALKING
+ WALKING_DOWNSTAIRS
+ WALKING_UPSTAIRS
+
---
+++ run_analysis.R
@@ -0,0 +1,94 @@
+run_analysis <- function(){
+  library(dplyr)
+  library(tidyr)
+  library(reshape2)
+  
+  #This function is designed to:
+  #1)import the data sets from:
+  #https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+  #2) Merges the training and the test sets to create one data set.
+  #3)Extracts only the measurements on the mean and standard deviation for each measurement.
+  #4)Uses descriptive activity names to name the activities in the data set
+  #Appropriately labels the data set with descriptive variable names.
+  #5)From the data set in step 4, creates a second, independent tidy data set with the
+  # average of each variable for each activity and each subject.
+  
+  #This section checks to see if a file called "getdata-projectfiles-UCI HAR Dataset.zip"
+  #exist in the workspace. If the file is not there it will be downloaded.
+  
+  if (file.exists("getdata-projectfiles-UCI HAR Dataset.zip") == TRUE){
+    rawdata <- unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+  }else{
+    print("Project Data Not Found, Downloading Data Now")
+    url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    file <- "getdata-projectfiles-UCI HAR Dataset.zip"
+    download.file(url,file, method = "curl", quiet = FALSE)
+    rawdata <- unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+  }
+  
+#this section extracts the data of interest
+  
+  #subject data
+  test.subj <- read.table(rawdata[14])
+  train.subj <- read.table(rawdata[26])
+   
+  #X data
+  test.x <- read.table(rawdata[15])
+  train.x <- read.table(rawdata[27])
+    
+  #Y data
+  test.y <- read.table(rawdata[16])
+  train.y <- read.table(rawdata[28])
+    
+#this section merges train and test data
+  
+  #subject
+  subj <- rbind(test.subj,train.subj)
+  
+  #X
+  x <- rbind(test.x,train.x)
+  
+  #Y
+  y <- rbind(test.y,train.y)
+  
+#this section renames the columns with the descriptive names found in the features file
+  v <- read.table(rawdata[2])
+  v <- v[,2]
+  names(x) <- v
+  
+  #rename subject data and Y data
+  names(subj) <-"Subject"
+  names(y) <- "Activity"
+  Activity <- y
+#this section uses the activity name data to modify the values of Y to the more descriptive activity
+#names found in activitylabels.txt file. 
+  
+  act <- read.table(rawdata[1])
+  act[,2] <- as.character(act[,2])
+  
+  #converts the Activity variable to a factor
+  
+  Activity <- unlist(Activity)
+  Activity <- factor(Activity, levels= act[,1], labels=act[,2])
+  
+
+#this section subsets the data to extract the std dev and mean columns
+  
+  x<-x[,grep(".*mean.*|.*std.*", v)]
+  
+#this section makes the column names more readable by replacing 
+#abreviations with words and removing extra characters
+  
+  names(x) <- gsub("-mean", " Mean ", names(x))
+  names(x) <- gsub("-std", " StanDev ", names(x))
+  names(x) <- gsub("()-", " ", names(x))
+  names(x) <- gsub("()", "", names(x), fixed=TRUE)
+ 
+    
+ 
+#this section merges Subject, Activity, and X data in that order
+ dat <- cbind(subj,Activity,x)
+
+
+
+}
